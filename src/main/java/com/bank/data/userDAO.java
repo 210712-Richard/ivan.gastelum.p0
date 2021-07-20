4e0bbec61442cb55c251f38a03f26110bede3766
igastelum <igastelum@miners.utep.edu>
2021-07-19 20:12:31 -0600

    added menus

--- src/main/java/com/bank/data/userDAO.java
+++ src/main/java/com/bank/data/userDAO.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.bank.beans.User;
+import com.bank.beans.UserType;
 
 public class userDAO {
 	//Database Access Object
@@ -18,7 +19,14 @@ public class userDAO {
 		
 		if(users==null) {
 			users = new ArrayList<User>();
-			users.add(new User(users.size(), ))
+			users.add(new User(users.size(), "agastelum", "123", "Andres", "Gastelum", "agastelum@example.com", LocalDate.of(1999, 3, 10)));
+			users.add(new User(users.size(), "kkenia", "456", "Kenia", "Guerrero", "kkenia@example.com", LocalDate.of(1994, 8, 19)));
+			users.add(new User(users.size(), "agarcia", "789", "Alex", "Garcia", "agarcia@example.com", LocalDate.of(1980, 5, 10)));
+			users.add(new User(users.size(), "rlomeli", "654", "Rocio", "Lomeli", "rlomeli@example.com", LocalDate.of(1975, 10, 15)));
+			User u = new User(users.size(), "igastelum", "123", "Ivan", "Gastelum", "igastelum@example.com", LocalDate.of(1994, 8, 7));
+			u.setType(UserType.ADMIN);
+			users.add(u);
+			ds.writeObjectsToFile(users, filename);
 		}
 	}
 	
--- src/main/java/com/bank/menu/Menu.java
+++ src/main/java/com/bank/menu/Menu.java
@@ -4,14 +4,64 @@ import java.util.Scanner;
 
 import com.bank.beans.User;
 import com.bank.services.UserService;
+import com.bank.util.SingletonScanner;
 
 public class Menu {
+	
+	//private static final Logger log = LogManager.getLogger(Menu.class);
+	
 	private UserService us = new UserService();
 	private User loggedUser = null;
-	
+	private Scanner scan = SingletonScanner.getScanner().getScan();
 	
 	public void start() {
-		//Function to start the menu for either logging in or create an account
+		//Function to start the app
+		mainLoop: while(true) {
+			switch(startMenu()) {
+				case 1:
+					//Log in
+					System.out.println("Please enter your username");
+					String username = scan.nextLine();
+					User u = us.login(username);
+					if (u==null) {
+						//log.warn("Unsuccessful login attempt: "+ username);
+						System.out.println("The username was not found. Please try again.");
+					}else {
+						loggedUser = u;
+						System.out.println("Welcome back: "+u.getUsername());
+						switch(loggedUser.getType()) {
+							case ADMIN:
+								adminMenu();
+								break;
+							case CUSTOMER:
+								customerMenu();
+								break;
+						}
+					}
+					break;
+				case 2:
+					//Register
+				case 3:
+					//Quit
+					System.out.println("Goodbye!");
+					break mainLoop;
+				default:
+					System.out.println("");
+			}
+		}
+	}
+	
+
+	private int startMenu() {
+		//Main menu for either logging in or create an account
+		System.out.println("Welcome to Goldmine Bank!");
+		System.out.println("Please select the one of the options:");
+		System.out.println("\t1. Login");
+		System.out.println("\t2. Register");
+		System.out.println("\t3. Quit");
+		int selection = select();
+		
+		return selection;
 	}
 	
 	private int customerMenu() {
@@ -23,6 +73,13 @@ public class Menu {
 	}
 	
 	private int select() {
-		return 1;//Selects an input
+		int selection;
+		try {
+			selection = Integer.parseInt(scan.nextLine());
+		} catch(Exception e) {
+			selection = -1;
+		}
+		//log
+		return selection;
 	}
 }
